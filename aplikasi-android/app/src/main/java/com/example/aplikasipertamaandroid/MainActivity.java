568ed36570e6b993bb0cd83dbae094824617365f
figuran04 <dikaelsaputra2406@gmail.com>
2025-05-17 14:02:41 +0700

    resolve: storage

--- aplikasi-android/app/src/main/java/com/example/aplikasipertamaandroid/MainActivity.java
+++ aplikasi-android/app/src/main/java/com/example/aplikasipertamaandroid/MainActivity.java
@@ -16,74 +16,74 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-  private EditText editText1, editText2;
-  private Spinner spinner;
-  private TextView textViewResult;
-  private Button buttonCalculate;
+    private EditText editText1, editText2;
+    private Spinner spinner;
+    private TextView textViewResult;
+    private Button buttonCalculate;
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-    // Inisialisasi komponen
-    editText1 = findViewById(R.id.editText1);
-    editText2 = findViewById(R.id.editTextNumber2);
-    spinner = findViewById(R.id.spinner);
-    textViewResult = findViewById(R.id.textView);
-    buttonCalculate = findViewById(R.id.button);
+        // Inisialisasi komponen
+        editText1 = findViewById(R.id.editText1);
+        editText2 = findViewById(R.id.editTextNumber2);
+        spinner = findViewById(R.id.spinner);
+        textViewResult = findViewById(R.id.textView);
+        buttonCalculate = findViewById(R.id.button);
 
-    // Set listener untuk window insets
-    ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-      Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-      v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-      return insets;
-    });
+        // Set listener untuk window insets
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
 
-    // Isi Spinner dengan operator matematika
-    ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, new String[]{"+", "-", "*", "/"});
-    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-    spinner.setAdapter(adapter);
+        // Isi Spinner dengan operator matematika
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, new String[]{"+", "-", "*", "/"});
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
 
 
-    // Set aksi tombol
-    buttonCalculate.setOnClickListener(v -> calculateResult());
-  }
-
-  private void calculateResult() {
-    String input1 = editText1.getText().toString();
-    String input2 = editText2.getText().toString();
-
-    if (input1.isEmpty() || input2.isEmpty()) {
-      Toast.makeText(this, "Masukkan kedua bilangan", Toast.LENGTH_SHORT).show();
-      return;
+        // Set aksi tombol
+        buttonCalculate.setOnClickListener(v -> calculateResult());
     }
 
-    double num1 = Double.parseDouble(input1);
-    double num2 = Double.parseDouble(input2);
-    String operator = spinner.getSelectedItem().toString();
-    double result = 0;
+    private void calculateResult() {
+        String input1 = editText1.getText().toString();
+        String input2 = editText2.getText().toString();
 
-    switch (operator) {
-      case "+":
-        result = num1 + num2;
-        break;
-      case "-":
-        result = num1 - num2;
-        break;
-      case "*":
-        result = num1 * num2;
-        break;
-      case "/":
-        if (num2 != 0) {
-          result = num1 / num2;
-        } else {
-          Toast.makeText(this, "Tidak dapat membagi dengan nol", Toast.LENGTH_SHORT).show();
-          return;
+        if (input1.isEmpty() || input2.isEmpty()) {
+            Toast.makeText(this, "Masukkan kedua bilangan", Toast.LENGTH_SHORT).show();
+            return;
         }
-        break;
-    }
 
-    textViewResult.setText(String.valueOf(result));
-  }
+        double num1 = Double.parseDouble(input1);
+        double num2 = Double.parseDouble(input2);
+        String operator = spinner.getSelectedItem().toString();
+        double result = 0;
+
+        switch (operator) {
+            case "+":
+                result = num1 + num2;
+                break;
+            case "-":
+                result = num1 - num2;
+                break;
+            case "*":
+                result = num1 * num2;
+                break;
+            case "/":
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    Toast.makeText(this, "Tidak dapat membagi dengan nol", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                break;
+        }
+
+        textViewResult.setText(String.valueOf(result));
+    }
 }
